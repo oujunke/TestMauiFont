50587fb0aac54c57d90f35ae6136960ccc7bb8a0
oujunke <oujunke@dfl.com.cn>
2023-05-09 08:52:54 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Graphics.Skia;
+using SkiaSharp;
+
+var fontPath = "BRITANIC.TTF";
+Font font = new Font(fontPath);
+var typeface=font.ToSKTypeface();
+var skTypeface=SKTypeface.FromFile(fontPath);
+Console.WriteLine(typeface.FamilyName);
+Console.WriteLine(skTypeface.FamilyName);
+Console.ReadLine();
+
